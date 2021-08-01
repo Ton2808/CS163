043925b23bba9672d85f69909f7cdcfc6c1102e6
A1QKT <taquangkhoi1232002@gmail.com>
2021-08-01 09:42:24 +0700

    Create Khoi's Trie structure

--- Khoi.h
+++ Khoi.h
@@ -1,4 +1,17 @@
 #ifndef Khoi_h
 #define Khoi_h
-
+#include "library.h"
+struct TrieNode
+{
+    TrieNode **children;
+    bool isEndNode;
+    vector<pair<string, int> > links;
+    TrieNode()
+    {
+        children = new TrieNode *[256];
+        for (int i = 0; i < 256; ++i)
+            children[i] = NULL;
+        isEndNode = false;
+    }
+};
 #endif
