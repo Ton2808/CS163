8d847f2d0e3a53a6929f2384fa2e394d6e944967
thanhduycao <81946359+thanhduycao@users.noreply.github.com>
2021-08-07 17:14:05 +0700

    Finish task 2; task 4 on progress

--- Duy.cpp
+++ Duy.cpp
@@ -14,7 +14,7 @@ bool checkOROperator(string inputString)
     return false;
 }
 
-vector<string> splitOROperator(TrieNode *root, string inputString)
+vector<string> splitOROperator(string inputString)
 {
     vector<string> res;
     string temp;
@@ -39,7 +39,7 @@ vector<string> splitOROperator(TrieNode *root, string inputString)
     return res;
 }
 
-void ranking_PlusOperator(TrieNode *root, vector<string> word, vector<string> &_5thLinks, int numberOfFiles)
+void ranking_OROperator(TrieNode *root, vector<string> word, vector<string> &_5thLinks, int numberOfFiles)
 {
     vector<pair<string, int> > *tmp;
     tmp = new vector<pair<string, int> >[word.size()];
@@ -48,60 +48,50 @@ void ranking_PlusOperator(TrieNode *root, vector<string> word, vector<string> &_
         searchInTrieNode(root, word[i], tmp[i]);
     }
 
-    map<string, int> checkAllWordIsInFile;
-    map<string, int> fwd;
-    //check how many word in file contained
-    for (int i = 0; i < word.size(); ++i)
-    {
-        for (int j = 0; j < tmp[i].size(); ++j)
-        {
-            if (fwd[tmp[i][j].first] < tmp[i][j].second)
-            {
-                fwd[tmp[i][j].first] = tmp[i][j].second;
-            }
-            checkAllWordIsInFile[tmp[i][j].first]++;
+    //Check if there are no document contain a or b then return 
+    bool check=false;
+    for (int i=0; i < word.size(); i++){
+        if (!tmp[i].empty()){
+            check=true;
+            break;
         }
     }
-    vector<string> store;
-    for (map<string, int>::iterator i = checkAllWordIsInFile.begin(); i != checkAllWordIsInFile.end(); ++i)
-    {
-        if (i->second == word.size())
-        {
-            store.push_back(i->first);
+    if (check==false) return;
+    //Calculate w for each document contained a, b and store link to string store
+    set<string> store;
+    map<string,float> w;
+    for (int i=0; i < word.size(); i++){
+        for (int j=0; j < tmp[i].size(); j++){
+            //if link has both a and b then we sum their w.
+            w[tmp[i][j].first] += tmp[i][j].second * log((float)numberOfFiles / tmp[i].size());
+            store.insert(tmp[i][j].first);
         }
     }
-    int fwD = store.size();
-    if (fwD)
-    {
-        map<string, float> w;
-        for (int i = 0; i < store.size(); ++i)
-        {
-            w[store[i]] = fwd[store[i]] * log((float)numberOfFiles / fwD);
-        }
-        bool *isLooped = new bool[store.size()];
-        for (int i = 0; i < store.size(); ++i)
-            isLooped[i] = false;
-        for (int i = 0; i < 5; ++i)
-        {
-            int max = -1;
-            int index = -1;
-            for (int j = 0; j < store.size(); ++j)
-            {
-                if (!isLooped[j])
-                {
-                    if (w[store[j]] > max)
-                    {
-                        index = j;
-                        max = w[store[j]];
-                    }
-                }
-            }
-            if (index != -1)
-            {
-                isLooped[index] = true;
-                _5thLinks.push_back(store[index]);
+    //Take 5th largest in w
+    
+    for (int i=0; i < 5; i++){
+        int max=-1;
+        set<string>::iterator itr, index;
+        for (itr=store.begin(); itr != store.end(); itr++){
+            if (w[*itr] > max){
+                max = w[*itr];
+                index = itr;
             }
         }
+        _5thLinks.push_back(*index);
+        store.erase(*index); 
     }
 }
+
+void activateOROperator(TrieNode *root, string inputString, int numberOfFiles)
+{
+    vector<string> listWords = splitOROperator(inputString);
+    vector<string> _5thLinks;
+    ranking_OROperator(root, listWords, _5thLinks, numberOfFiles);
+    print(listWords, _5thLinks);
+}
+
+//Task 4: intitle:a b
+//in title have a and b
+//before intitle is in every where
 
